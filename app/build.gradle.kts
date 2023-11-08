f0cacfc24f72f128c2cb7a9a7b329dce8cabc03e
winiger <griederl@bzz.ch>
2023-11-08 11:05:09 +0100

    DelRoso

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,12 +4,12 @@ plugins {
 
 android {
     namespace = "com.example.pennywise"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.pennywise"
         minSdk = 24
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
--- app/src/main/java/com/example/pennywise/MainActivity.java
+++ app/src/main/java/com/example/pennywise/MainActivity.java
@@ -1,14 +1,39 @@
 package com.example.pennywise;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ListView;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
+
+public class MainActivity extends Activity {
+    private ArrayList<String> expensesList;
+    private ArrayAdapter<String> expensesAdapter;
+    private EditText expenseInput;
+    private ListView expensesListView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        expensesList = new ArrayList<>();
+        expensesAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, expensesList);
+        expenseInput = findViewById(R.id.expenseInput);
+        expensesListView = findViewById(R.id.expensesList);
+
+        expensesListView.setAdapter(expensesAdapter);
+    }
+
+    public void addExpense(View view) {
+        String expense = expenseInput.getText().toString();
+        if (!expense.isEmpty()) {
+            expensesList.add(expense);
+            expensesAdapter.notifyDataSetChanged();
+            expenseInput.setText("");
+        }
     }
 }
