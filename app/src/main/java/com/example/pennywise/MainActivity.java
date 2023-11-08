c632751c88f26ec9719b5a3476fe673ee5d7fca6
Robin-Memobar <96288924+Robin-Memobar@users.noreply.github.com>
2023-11-08 13:46:59 +0100

    verpiss dich aleks

--- app/src/main/java/com/example/pennywise/MainActivity.java
+++ app/src/main/java/com/example/pennywise/MainActivity.java
@@ -1,39 +1,71 @@
-package com.example.pennywise;
+package com.example.pennywise; // Ändern Sie dies entsprechend dem Paketnamen Ihrer App
 
-import android.app.Activity;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.ArrayAdapter;
-import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.TextView;
+import androidx.appcompat.app.AppCompatActivity;
 
-import java.util.ArrayList;
+import com.example.pennywise.R;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
-public class MainActivity extends Activity {
-    private ArrayList<String> expensesList;
-    private ArrayAdapter<String> expensesAdapter;
-    private EditText expenseInput;
-    private ListView expensesListView;
+public class MainActivity extends AppCompatActivity {
+
+    private TextView textViewCurrentBudget;
+    private ListView listViewLastChanges;
+    private BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        expensesList = new ArrayList<>();
-        expensesAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, expensesList);
-        expenseInput = findViewById(R.id.expenseInput);
-        expensesListView = findViewById(R.id.expensesList);
+        // TextView für das aktuelle Budget initialisieren
+        textViewCurrentBudget = findViewById(R.id.textViewCurrentBudget);
+        // ListView für die letzten Änderungen initialisieren
+        listViewLastChanges = findViewById(R.id.listViewLastChanges);
+        // BottomNavigationView initialisieren
+        bottomNavigationView = findViewById(R.id.bottom_navigation);
 
-        expensesListView.setAdapter(expensesAdapter);
+        // Setzen Sie hier den aktuellen Budgetbetrag aus Ihrer Datenquelle
+        updateCurrentBudget(9123.00); // Beispielwert
+
+        // Ein ArrayAdapter, um die letzten Änderungen anzuzeigen
+        String[] lastChanges = getLastChanges();
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, lastChanges);
+        listViewLastChanges.setAdapter(adapter);
+
+        bottomNavigationView = findViewById(R.id.bottom_navigation);
+        // Listener für die BottomNavigationView hinzufügen
+        bottomNavigationView.setOnNavigationItemSelectedListener(item -> {
+            int id = item.getItemId();
+            if (id == R.id.navigation_home) {
+                // Hier Code einfügen, um das Home-Fragment oder die Home-Activity anzuzeigen
+            } else if (id == R.id.navigation_budget) {
+                // Hier Code einfügen, um das Budgetplan-Fragment oder die Budgetplan-Activity anzuzeigen
+            } else if (id == R.id.navigation_analysis) {
+                // Hier Code einfügen, um das Analyse-Fragment oder die Analyse-Activity anzuzeigen
+            } else if (id == R.id.navigation_help) {
+                // Hier Code einfügen, um das Hilfe-Fragment oder die Hilfe-Activity anzuzeigen
+            }
+
+            return true; // true gibt an, dass die Item-Auswahl behandelt wurde
+        });
     }
 
-    public void addExpense(View view) {
-        String expense = expenseInput.getText().toString();
-        if (!expense.isEmpty()) {
-            expensesList.add(expense);
-            expensesAdapter.notifyDataSetChanged();
-            expenseInput.setText("");
-        }
+    private void updateCurrentBudget(double budget) {
+        // Formatieren Sie den Betrag und setzen Sie den Text
+        String budgetText = String.format("Aktuelles Budget: %.2f", budget);
+        textViewCurrentBudget.setText(budgetText);
+    }
+
+    private String[] getLastChanges() {
+        // Dies würde die letzten Änderungen von Ihrer Datenquelle holen
+        // Hier ist ein Platzhalter-Beispiel
+        return new String[] {
+                "Stromrechnung - 300",
+                "Budgeterhöhung - 1000",
+                "Gym-Abonnement - 54"
+        };
     }
 }
