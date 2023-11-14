de7144c6460555469d549dec2166f988879077e7
Robin-Memobar <96288924+Robin-Memobar@users.noreply.github.com>
2023-11-14 13:38:37 +0100

    doppeltechatgpt123

--- app/src/main/java/com/example/pennywise/Analyse.java
+++ app/src/main/java/com/example/pennywise/Analyse.java
@@ -3,6 +3,7 @@ package com.example.pennywise;
 import android.animation.AnimatorSet;
 import android.animation.ObjectAnimator;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -24,15 +25,15 @@ public class Analyse extends AppCompatActivity {
     private ListView listViewLastChanges;
     private BottomNavigationView bottomNavigationView;
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.analyse);
 
-        householdamount = findViewById(R.id.householdAmount);
-        foodAmount = findViewById(R.id.foodAmount);
-        hobbyAmount = findViewById(R.id.hobbyAmount);
-        restAmount = findViewById(R.id.restAmount);
+        // Lade und zeige die Beträge an
+        loadAndDisplayAmounts();
+
 
         final Button buttonHome = findViewById(R.id.navigation_home);
         buttonHome.setOnClickListener(new View.OnClickListener() {
@@ -61,6 +62,29 @@ public class Analyse extends AppCompatActivity {
                 buttonHelpActivity(v);
             }
         });
+
+
+
+    }
+    private void loadAndDisplayAmounts() {
+        SharedPreferences sharedPreferences = getSharedPreferences("BudgetPrefs", MODE_PRIVATE);
+
+        // Werte aus SharedPreferences abrufen
+        int haushaltBetrag = sharedPreferences.getInt("AusgabenHaushalt", 0);
+        int essenBetrag = sharedPreferences.getInt("AusgabenEssen", 0);
+        int freizeitBetrag = sharedPreferences.getInt("AusgabenFreizeit", 0);
+        int sonstigesBetrag = sharedPreferences.getInt("AusgabenSonstiges", 0);
+
+        householdamount = findViewById(R.id.householdAmount);
+        foodAmount = findViewById(R.id.foodAmount);
+        hobbyAmount = findViewById(R.id.hobbyAmount);
+        restAmount = findViewById(R.id.restAmount);
+
+        // Werte in den TextViews anzeigen
+        householdamount.setText(String.format("%d", haushaltBetrag));
+        foodAmount.setText(String.format("%d", essenBetrag));
+        hobbyAmount.setText(String.format("%d", freizeitBetrag));
+        restAmount.setText(String.format("%d", sonstigesBetrag));
     }
 
     private void buttonHomeClicked(View view) {
